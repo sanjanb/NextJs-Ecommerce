643150730e77aa2c28e3a20f1cbb2ea2ab253fb5
Sanjan B M <acharyasanjan931@gmail.com>
2025-02-21 16:32:23 +0530

    added Product Image component

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -37,7 +37,7 @@ const ProductImages = ({ items }: { items: any }) => {
         />
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {items.map((item:any, i:number) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
             key={item._id}
